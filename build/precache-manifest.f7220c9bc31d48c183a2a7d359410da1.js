6da2d7cd922f247ed24992b37c7cb58d8424e666
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-22 11:17:31 +0800

    Master from heroku merged with master in Github

---
+++ build/precache-manifest.f7220c9bc31d48c183a2a7d359410da1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39f1a0b12259674bdd2daaea524b227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60cdd033b506d01e7964",
+    "url": "/static/js/2.63954bc2.chunk.js"
+  },
+  {
+    "revision": "0b94642a9af59c7beb94",
+    "url": "/static/js/main.59f46f5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
