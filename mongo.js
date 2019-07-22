e84ab8d8bfe90d4e123617daf5a62c983b7ca8c5
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-22 10:11:27 +0800

    Commit for push to heroku remote

---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length < 3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://admin:${password}@learn-mern-stack-nreww.gcp.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: false,
+})
+
+// note.save().then(response => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    console.log(result.length)
+    mongoose.connection.close()
+})
