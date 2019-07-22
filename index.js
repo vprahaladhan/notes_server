9ab4be0214c4b27e0f23a88c0a443f767ac9f155
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-22 11:37:52 +0800

    Changes made after ex part 3.12

--- index.js
+++ index.js
@@ -8,49 +8,46 @@ app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(cors())
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML/CSS is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    },
-    {
-        id: 4,
-        content: "Javascript is powerful",
-        date: "2019-05-30T19:20:14.298Z",
-        important: false
-    }
-]
+const mongoose = require('mongoose')
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
-  
-app.get('/notes', (req, res) => {
-    res.json(notes)
+const password = 'admin'
+
+const url =
+  `mongodb+srv://admin:${password}@learn-mern-stack-nreww.gcp.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
-app.get('/notes/:id', (req, res) => {
-    res.json(notes.find(note => note.id == req.params.id))
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: false,
+})
+
+app.get('/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+        mongoose.connection.close()
+    })
+})
+
+app.get('/notes/:id', (request, response) => {
+    response.json(notes.find(note => note.id == request.params.id))
 })
 
 app.post('/notes', (request, response) => {
-    request.body.id = Math.max(...notes.map(note => note.id)) + 1
-    notes = notes.concat(request.body)
-    response.json(request.body)
+    note.save().then(response => {
+        console.log('note saved!')
+        response.json(request.body)
+        mongoose.connection.close()
+    })
 })
 
 app.put('/notes/:id', (request, response) => {
